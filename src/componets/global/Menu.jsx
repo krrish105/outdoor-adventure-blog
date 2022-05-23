33f7e3683d1c76d91fd489851103b4bbb8153a6f
Krrish105 <jainkarishma76@gmail.com>
2022-05-23 15:37:19 +0530

    fixed 2 issues in mobile navigation menu and also updated the favicon

--- src/componets/global/Menu.jsx
+++ src/componets/global/Menu.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Menu = () => {
+const Menu = ({menuOpen, setMenuOpen}) => {
     return (
         <div className="nav__menu">
             <ul className="nav__list">
-                <li className="nav__item">
+                <li className="nav__item" onClick={() => setMenuOpen(!menuOpen)}>
                     <Link to="/" className="nav__link">Home</Link>
                 </li>
-                <li className="nav__item">
+                <li className="nav__item" onClick={() => setMenuOpen(!menuOpen)}>
                     <Link to="/about" className="nav__link">About</Link>
                 </li>
-                <li className="nav__item">
+                <li className="nav__item" onClick={() => setMenuOpen(!menuOpen)}>
                     <Link to="/services" className="nav__link">Services</Link>
                 </li>
 
